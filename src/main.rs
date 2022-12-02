662e69220b5ec64a7da7f1900cae2375152267bb
Evan Cowin <cowin.6@buckeyemail.osu.edu>
2022-12-02 12:41:08 -0500

    removed prints

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ pub mod commands;
 
 
 use std::sync::Arc;
+use std::env;
 
 // This trait adds the `register_songbird` and `register_songbird_with` methods
 // to the client builder below, making it easy to install this voice client.
